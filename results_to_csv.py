18b98f9999121f6890a4cd084a917716cd7888bf
Justin Penney <justin@justinpenney.com>
2018-02-10 18:02:39 -0600

    Pulling in by url on command line. Error checking for missing days.

--- results_to_csv.py
+++ results_to_csv.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+import urllib2
 import re
+import sys
 
-with open('sample_output.html') as f:
-    soup = BeautifulSoup(f, 'html.parser')
-
+#with open('sooner.html') as f:
+#    soup = BeautifulSoup(f, 'html.parser')
+url = sys.argv[-1]
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
 groups = soup.findAll('h4', class_='race-result-group')
 
 days = soup.findAll('div', id=re.compile('^rday_'))
@@ -13,7 +17,10 @@ days = soup.findAll('div', id=re.compile('^rday_'))
 print 'Race, Class, Place, Name, City, State, Sponsor'
 for day in days:
     title = day.find('h3', class_='race-result-title')
-    title = title.text
+    try:
+        title = title.text
+    except:
+        break
     # print 'RACE', title
 
     uls = day.findAll('ul', class_='race-result-list')
@@ -36,11 +43,12 @@ for day in days:
                 rider_name = rider_info[0]
                 rider_city = rider_info[-2]
                 rider_state = rider_info[-1]
+                if rider_state == ' MO' or rider_state == ' KS':
+                    #print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
+                    print '{}, {}, {}'.format(rider_name, place.text, class_name)
             else:
                 pass
 
-            if rider_state == ' MO' or rider_state == ' KS':
-                print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
 
         count += 1
 
