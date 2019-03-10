7045fe369ac39dba92fc36a45c65f12ac938bc25
Justin Penney <justin@justinpenney.com>
2019-03-10 12:15:06 -0500

    Fixed error on missing days.

--- results_to_csv.py
+++ results_to_csv.py
@@ -4,29 +4,44 @@ from bs4 import BeautifulSoup
 import urllib2
 import re
 import sys
+import collections
 
-#with open('sooner.html') as f:
+#with open('grands.html') as f:
 #    soup = BeautifulSoup(f, 'html.parser')
 url = sys.argv[-1]
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-# groups = soup.findAll('h4', class_='race-result-group')
+groups = soup.findAll('h4', class_='race-result-group')
+
+#foo = dir(page)
+#bar = page.__dict__
+#print foo
+#print bar
 
 days = soup.findAll('div', id=re.compile('^rday_'))
 
 #print 'Race, Class, Place, Name, City, State, Sponsor'
-print 'Name, Place, Class'
 for day in days:
     title = day.find('h3', class_='race-result-title')
+    
+    if title is None:
+        exit(0)
 
     title = title.text
 
-    print '\n\n\nRACE: {}, ,'.format(title)
+    print '\n\n----------------------------------'
+    print 'RACE: {}'.format(title)
+    print '----------------------------------'
 
     groups = day.findAll('h4', class_='race-result-group')
     uls = day.findAll('ul', class_='race-result-list')
     count = 0
-    places = dict()
+    places = collections.OrderedDict()
+    p = 1
+    while p < 8:
+        places[p] = 0
+        p += 1
+
     for ul in uls:
         group = groups[count].text
         class_name = group.split('Total Riders')[0].rstrip()
@@ -38,9 +53,9 @@ for day in days:
 
             if rider_info_len >= 3:
                 if rider_info_len == 4:
-                  rider_sponsor = rider_info[1]
+                  rider_sponsor = rider_info[1].lstrip()
                 else:
-                    rider_sponsor = 'Privateer'
+                  rider_sponsor = None
 
                 rider_name = rider_info[0]
                 rider_city = rider_info[-2]
@@ -48,17 +63,26 @@ for day in days:
                 if rider_state == ' MO' or rider_state == ' KS':
                     #print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
                     try:
-                        places[str(place.text)] += 1
+                        places[int(place.text)] += 1
                     except:
-                        places[str(place.text)] = 1
-
-                    print '{}, {}, {}'.format(rider_name, place.text, class_name)
+                        places[int(place.text)] = 1
+                   
+                    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+                    place_ordinal = ordinal(int(place.text))
+                    #print '{}, {}, {}'.format(rider_name, place.text, class_name)
+                    if rider_sponsor:
+                        try:
+                            rider_name = "{} ({})".format(rider_name, str(rider_sponsor))
+                        except:
+                            pass
+                    print '{} - {} in {}'.format(rider_name, place_ordinal, class_name)
             else:
                 pass
 
 
         count += 1
-    print '\n\nPlace Count:'
+    print '\nPlace Count:'
     for k,v in places.iteritems():
-        print '{} - {}'.format(k,v)
+        if v > 0:
+            print '{} - {}'.format(k,v)
 
