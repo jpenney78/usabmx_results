27a85a4483e4fdf657ce75f2bb3349b3b71118cf
Justin Penney <jnpenney@burnsmcd.com>
2020-08-09 21:01:03 -0500

    feat: strider places changed

--- results_to_csv.py
+++ results_to_csv.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
-import urllib2
+import urllib3
 import re
 import sys
 import collections
 
 #with open('grands.html') as f:
 #    soup = BeautifulSoup(f, 'html.parser')
-url = sys.argv[-1]
-page = urllib2.urlopen(url)
+#url = sys.argv[-1]
+url = 'https://www.usabmx.com/site/bmx_races/422419/results?past_only=1&section_id=228'
+http = urllib3.PoolManager()
+r = http.request('GET', url)
+page = r.data
+#print(page)
+#exit(0)
 soup = BeautifulSoup(page, 'html.parser')
 groups = soup.findAll('h4', class_='race-result-group')
 
@@ -29,9 +34,9 @@ for day in days:
 
     title = title.text
 
-    print '\n\n----------------------------------'
-    print 'RACE: {}'.format(title)
-    print '----------------------------------'
+    print('\n\n----------------------------------')
+    print('RACE: {}'.format(title))
+    print('----------------------------------')
 
     groups = day.findAll('h4', class_='race-result-group')
     uls = day.findAll('ul', class_='race-result-list')
@@ -62,27 +67,31 @@ for day in days:
                 rider_state = rider_info[-1]
                 if rider_state == ' MO' or rider_state == ' KS':
                     #print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
+                    place_text = place.text
+                    if place_text == 'O' or place_text == 'S':
+                        place_text = 1
                     try:
-                        places[int(place.text)] += 1
+                        places[int(place_text)] += 1
                     except:
-                        places[int(place.text)] = 1
+                        places[int(place_text)] = 1
                    
                     ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-                    place_ordinal = ordinal(int(place.text))
+                    place_ordinal = ordinal(int(place_text))
                     #print '{}, {}, {}'.format(rider_name, place.text, class_name)
                     if rider_sponsor:
                         try:
                             rider_name = "{} ({})".format(rider_name, str(rider_sponsor))
                         except:
                             pass
-                    print '{} - {} in {}'.format(rider_name, place_ordinal, class_name)
+                    print('{} - {} in {}'.format(rider_name, place_ordinal, class_name))
             else:
                 pass
 
 
         count += 1
-    print '\nPlace Count:'
-    for k,v in places.iteritems():
-        if v > 0:
-            print '{} - {}'.format(k,v)
+    print('\nPlace Count:')
+    for i in places:
+        if places[i] > 0:
+            print(f"{i} - {places[i]}")
+        #    print('{} - {}'.format(k,v))
 
