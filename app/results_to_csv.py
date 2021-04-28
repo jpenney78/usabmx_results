bd440ccd7deb101848d16796dd2304989af73c63
Justin Penney <justin.penney@1898andco.com>
2021-04-28 12:07:09 -0500

    functioning

--- app/results_to_csv.py
+++ app/results_to_csv.py
@@ -1,87 +1,88 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
-import urllib2
+import urllib3
 import re
 import sys
 import collections
-import cgi, cgitb
 
-form = cgi.FieldStorage()
-results_url = form.getvalue('results_url')
-states = form.getvalue('states')
-state_list = states.split(',')
-results_url = sys.argv[-1]
-page = urllib2.urlopen(results_url)
-soup = BeautifulSoup(page, 'html.parser')
+def get_results(url):
+    # url = 'https://www.usabmx.com/site/bmx_races/448450/results?past_only=1&section_id=228'
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    page = r.data
+    soup = BeautifulSoup(page, 'html.parser')
+    groups = soup.findAll('h4', class_='race-result-group')
 
-groups = soup.findAll('h4', class_='race-result-group')
+    days = soup.findAll('div', id=re.compile('^rday_'))
+    res = str()
 
-days = soup.findAll('div', id=re.compile('^rday_'))
+    for day in days:
+        title = day.find('h3', class_='race-result-title')
+        
+        if title is None:
+            exit(0)
 
-#print 'Race, Class, Place, Name, City, State, Sponsor'
-for day in days:
-    title = day.find('h3', class_='race-result-title')
-    
-    if title is None:
-        exit(0)
+        title = title.text
 
-    title = title.text
+        res += '\n\n----------------------------------\n'
+        res += 'RACE: {}\n'.format(title)
+        res += '----------------------------------\n'
 
-    print '\n\n----------------------------------'
-    print 'RACE: {}'.format(title)
-    print '----------------------------------'
+        groups = day.findAll('h4', class_='race-result-group')
+        uls = day.findAll('ul', class_='race-result-list')
+        count = 0
+        places = collections.OrderedDict()
+        p = 1
+        while p < 8:
+            places[p] = 0
+            p += 1
 
-    groups = day.findAll('h4', class_='race-result-group')
-    uls = day.findAll('ul', class_='race-result-list')
-    count = 0
-    places = collections.OrderedDict()
-    p = 1
-    while p < 8:
-        places[p] = 0
-        p += 1
+        for ul in uls:
+            group = groups[count].text
+            class_name = group.split('Total Riders')[0].rstrip()
+            for li in ul.findAll('li'):
+                (place, rider) = li.findAll('span')
+                rider_info = rider.text.rstrip().split(',')
+                rider_info_len = len(rider_info)
 
-    for ul in uls:
-        group = groups[count].text
-        class_name = group.split('Total Riders')[0].rstrip()
-        # print class_name
-        for li in ul.findAll('li'):
-            (place, rider) = li.findAll('span')
-            rider_info = rider.text.rstrip().split(',')
-            rider_info_len = len(rider_info)
+                if rider_info_len >= 3:
+                    if rider_info_len == 4:
+                        rider_sponsor = rider_info[1].lstrip()
+                    else:
+                        rider_sponsor = None
 
-            if rider_info_len >= 3:
-                if rider_info_len == 4:
-                  rider_sponsor = rider_info[1].lstrip()
-                else:
-                  rider_sponsor = None
-
-                rider_name = rider_info[0]
-                rider_city = rider_info[-2]
-                rider_state = rider_info[-1]
-                if rider_state == ' MO' or rider_state == ' KS':
-                    #print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
-                    try:
-                        places[int(place.text)] += 1
-                    except:
-                        places[int(place.text)] = 1
-                   
-                    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-                    place_ordinal = ordinal(int(place.text))
-                    #print '{}, {}, {}'.format(rider_name, place.text, class_name)
-                    if rider_sponsor:
+                    rider_name = rider_info[0]
+                    rider_city = rider_info[-2]
+                    rider_state = rider_info[-1]
+                    if rider_state == ' MO' or rider_state == ' KS':
+                        #print '{},{},{},{},{},{},{}'.format(title, class_name, place.text, rider_name, rider_city, rider_state, rider_sponsor)
+                        place_text = place.text
+                        if place_text == 'O' or place_text == 'S':
+                            place_text = 1
                         try:
-                            rider_name = "{} ({})".format(rider_name, str(rider_sponsor))
+                            places[int(place_text)] += 1
                         except:
-                            pass
-                    print '{} - {} in {}'.format(rider_name, place_ordinal, class_name)
-            else:
-                pass
+                            places[int(place_text)] = 1
+                    
+                        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+                        place_ordinal = ordinal(int(place_text))
+                        #print '{}, {}, {}'.format(rider_name, place.text, class_name)
+                        if rider_sponsor:
+                            try:
+                                rider_name = "{} ({})".format(rider_name, str(rider_sponsor))
+                            except:
+                                pass
+                        res += '{} - {} in {}\n'.format(rider_name, place_ordinal, class_name)
+                else:
+                    pass
 
 
-        count += 1
-    print '\nPlace Count:'
-    for k,v in places.iteritems():
-        if v > 0:
-            print '{} - {}'.format(k,v)
+            count += 1
+        res += '\nPlace Count:\n'
+        for i in places:
+            if places[i] > 0:
+                res += f"{i} - {places[i]}\n"
+
+    return res
 
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,25 @@
-from flask import render_template
+from flask import render_template, flash, request, redirect
 from app import app
 from app.forms import RequestForm
+from app.results_to_csv import get_results
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    url = {'url': 'URL'}
-    return render_template('index.html', title='Results Form', url=url)
-
-
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @app.route('/request', methods=['GET', 'POST'])
-def request():
-    form = RequestForm()
-    if form.validate_on_submit():
-        flash('Url={}, States={}'.format(
-            form.results_url.data, form.state_list.data))
-        return redirect('/index')
-    return render_template('request.html', title='Get Results', form=form)
+def request_results():
+    if request.method == 'POST':
+        req = request.form
+        results = get_results(req.get('results_url'))
+        return render_template('results.html', title='Results', results=results)
+    else:
+        form = RequestForm()
+        if form.validate_on_submit():
+            flash('Url={}, States={}'.format(
+                form.results_url.data, form.state_list.data))
+            return redirect('/index')
+        return render_template('request.html', title='Get Results', form=form)
+
+#def index():
+#    url = {'url': 'URL'}
+#    return render_template('index.html', title='Results Form', url=url)
